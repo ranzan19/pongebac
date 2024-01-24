fccfbef633353f04781381d9aa92a1dc4051e399
ranzan19 <rafaspirina@gmail.com>
2024-01-23 22:34:15 -0300

    desenvolvimento pong
    construção do jogo "pong"

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    // No script do GameManager
+    public Transform playerPaddle;
+    public Transform enemyPaddle;
+
+    public BallController ballController;
+
+    public int playerScore = 0;
+    public int enemyScore = 0;
+
+    public TextMeshProUGUI textPointsPlayer;
+    public TextMeshProUGUI textPointsEnemy;
+
+    public int winPoints = 3;
+
+    private void Start()
+    {
+        ResetGame();
+    }
+
+    public void ResetGame()
+    {
+        // Define as posições iniciais das raquetes
+        playerPaddle.position = new Vector3(-7f, 0f, 0f);
+        enemyPaddle.position = new Vector3(7f, 0f, 0f);
+        // ...
+
+        ballController.ResetBall();
+
+        playerScore = 0;
+        enemyScore = 0;
+
+        textPointsEnemy.text = enemyScore.ToString();
+        textPointsPlayer.text = playerScore.ToString();
+
+    }
+
+    public void ScorePlayer()
+    {
+        playerScore++;
+        textPointsPlayer.text = playerScore.ToString();
+        CheckWin();
+    }
+
+    public void ScoreEnemy()
+    {
+        enemyScore++;
+        textPointsEnemy.text = enemyScore.ToString();
+        CheckWin();
+    }
+
+    public void CheckWin()
+    {
+        if (enemyScore >= winPoints || playerScore >= winPoints)
+        {
+            ResetGame();
+        }
+    }
+
+}
